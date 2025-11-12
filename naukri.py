0d2730b924bc5cb973cc04609bfa3004aa67451a
Vijay Ausare <vijay.ausare207@gmail.com>
2025-11-12 23:30:42 +0530

    Update naukri.py

--- naukri.py
+++ naukri.py
@@ -23,6 +23,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service as ChromeService
 import constants
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+def get_driver(headless=True):
+    options = Options()
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    if headless:
+        options.add_argument("--headless=new")
+    driver = get_driver(headless=True)
+    return driver
+
+
 # Add folder Path of your resume
 originalResumePath = constants.NAUKRI_ORIGINAL_RESUME_PATH
 # Add Path where modified resume should be saved
