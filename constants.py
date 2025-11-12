1024317ddf15b2537b0e3fab87912f6287f061d9
Vijay Ausare <vijay.ausare207@gmail.com>
2025-11-12 22:31:07 +0530

    Update constants.py
    Updated const.py

--- constants.py
+++ constants.py
@@ -1,9 +1,10 @@
 # Configuration and constants used in the script. Update values as needed.
+import os
 
-USERNAME = "your_naukri_username"
-PASSWORD = "your_naukri_password"
-MOBILE = "your_naukri_phone"
-ORIGINAL_RESUME_PATH="your-resume-file-path/Resume.pdf"
-MODIFIED_RESUME_PATH="modified-resume-file-path/Resume.pdf"
-NAUKRI_LOGIN_URL="https://www.naukri.com/nlogin/login"
-NAUKRI_PROFILE_URL="https://www.naukri.com/mnjuser/profile"
+NAUKRI_USERNAME = os.environ.get("NAUKRI_USERNAME")
+NAUKRI_PASSWORD = os.environ.get("NAUKRI_PASSWORD")
+NAUKRI_MOBILE = os.environ.get("NAUKRI_MOBILE")
+NAUKRI_ORIGINAL_RESUME_PATH = os.environ.get("NAUKRI_ORIGINAL_RESUME_PATH")
+NAUKRI_MODIFIED_RESUME_PATH = os.environ.get("NAUKRI_MODIFIED_RESUME_PATH")
+NAUKRI_LOGIN_URL = os.environ.get("NAUKRI_LOGIN_URL")
+NAUKRI_PROFILE_URL = os.environ.get("NAUKRI_PROFILE_URL")
